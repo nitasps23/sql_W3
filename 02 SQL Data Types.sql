88149d56e3170f6b7f23dce20f1d480ceffaa7b5
Nita Sokphoodsa <153405782+nitasps23@users.noreply.github.com>
2024-12-22 05:34:01 +0100

    first commit

---
+++ 02 SQL Data Types.sql
@@ -0,0 +1,15 @@
+--SQL Data Types for MySQL, SQL Server, and MS Access
+--The data type of a column defines what value the column can hold: integer, character, money, date and time, binary, and so on.
+
+--SQL Data Types
+--Each column in a database table is required to have a name and a data type.
+
+--An SQL developer must decide what type of data that will be stored inside each column when creating a table. 
+--The data type is a guideline for SQL to understand what type of data is expected inside of each column, and it also identifies how SQL will interact with the stored data.
+
+--Note: Data types might have different names in different database. And even if the name is the same, the size and other details may be different! Always check the documentation!
+
+--MySQL Data Types (Version 8.0)
+--In MySQL there are three main data types: string, numeric, and date and time.
+
+--Link: https://pathfinder.w3schools.com/learningpaths?skillId=9cbcc948-ef85-43af-bc45-126225c89ec5
