5c37633e22e3615422ffaa2cf90fda3f5614c50e
Nita Sokphoodsa <153405782+nitasps23@users.noreply.github.com>
2024-12-29 05:13:44 +0100

    first commit

---
+++ 05 SQL SELECT Statement.sql
@@ -0,0 +1,28 @@
+--SQL SELECT Statement
+
+/*The SQL SELECT Statement
+The SELECT statement is used to select data from a database.
+
+Example
+Return data from the Customers table:*/
+
+SELECT CustomerName, City 
+FROM Customers;
+
+/*Syntax
+SELECT column1, column2, ...
+FROM table_name;
+
+Here, column1, column2, ... are the field names of the table you want to select data from.
+
+The table_name represents the name of the table you want to select data from.
+*/
+
+/*Select ALL columns
+If you want to return all columns, without specifying every column name, you can use the SELECT * syntax:
+
+Example
+Return all the columns from the Customers table:*/
+
+SELECT *
+FROM Customers;
